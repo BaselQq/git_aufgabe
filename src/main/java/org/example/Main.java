64e95e18194cab06e054aaeeabf85725952074d1
admin <baselqarb@gmail.com>
2024-03-26 10:48:51 +0100

    add feature 1 more changes!

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("feature1 changes");
+        System.out.println("more feature1 changes");
     }
 }
